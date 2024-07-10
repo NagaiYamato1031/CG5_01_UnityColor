fb8ca852d75df0068bf370bea107e1b5133c8f0f
NagaiYamato <115054936+NagaiYamato1031@users.noreply.github.com>
2024-07-10 11:19:06 +0900

    課題完了

--- Assets/HSVScript.cs
+++ Assets/HSVScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class HSVScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[Range(0.0f, 1.0f)]
+	public float H = 0.0f, S = 1.0f, V = 1.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private Material material;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		material = GetComponent<Renderer>().material;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		material.color = Color.HSVToRGB(H, S, V);
+	}
 }
